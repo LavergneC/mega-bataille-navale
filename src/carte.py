5ee26cae3bae724ebe172ffd29913b5c1f2d730e
nicolas_gibaud <n.gibaud@catie.fr>
2020-02-13 15:35:15 +0100

    Change the check_ship method and add new method to update case state

--- src/carte.py
+++ src/carte.py
@@ -21,12 +21,15 @@ class Carte:
 
     def check_ship(self, x, y, z):
         """Vérifie si un bateau est présent à l'endroit de la carte."""
+        for index, case in enumerate(self.cases):
+            if case.x == x and case.y == y and case.z == z:
+                self.cases[index].impact = True
+        self.mise_a_jour_case(x, y, z)
+
+    def mise_a_jour_case(self, x, y, z):
         for navire in self.navires:
             case_touche, etat_attaque = navire.test_impact(x, y, z)
             if etat_attaque:
-                for index, case in enumerate(self.cases):
-                    if case.index == case_touche.index:
-                        self.cases[index].impact = True
                 return True
         return False
 
