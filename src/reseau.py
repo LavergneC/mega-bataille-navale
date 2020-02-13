5c18a8936bff21f38565978025ebd18d14ea8634
nicolas_gibaud <n.gibaud@catie.fr>
2020-02-13 18:16:44 +0100

    Fix some bugs

--- src/reseau.py
+++ src/reseau.py
@@ -4,17 +4,18 @@ class Reseau:
 
     def __init__(self):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.port = "12800"
+        self.port = 12800
 
     def get_ip(self):
-        hostname = self.socket.gethostname()
-        self.ip_address = self.socket.gethostbyname(hostname)
+        hostname = socket.gethostname()
+        self.ip_address = socket.gethostbyname(hostname)
 
     def se_connecter(self, ip, port):
-        self.socket.connect(host=self.ip_address, port=port, timeout=1000)
+        self.socket.connect((ip, port))
 
     def heberger(self):
         self.infos_connexion = (self.ip_address, self.port)
         self.socket.bind(self.infos_connexion)
+        self.socket.listen(2)
         socketclient, self.infos_connexion = self.socket.accept()
         print(self.infos_connexion)
