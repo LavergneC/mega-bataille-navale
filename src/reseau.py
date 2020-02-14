e8508b27473ab21584e0ad909e62ba097a6e87c1
DvAx <devaux25@gmail.com>
2020-02-14 11:00:06 +0100

    ajout com serveur et com client

--- src/reseau.py
+++ src/reseau.py
@@ -17,5 +17,12 @@ class Reseau:
         self.infos_connexion = (self.ip_address, self.port)
         self.socket.bind(self.infos_connexion)
         self.socket.listen(2)
-        socketclient, self.infos_connexion = self.socket.accept()
+        self.socketclient, self.infos_connexion = self.socket.accept()
         print(self.infos_connexion)
+
+    def communication_cote_serveur(self):
+        self.socketclient.send(b"Je viens d'accepter  la connexion")
+
+    def communication_cote_client(self):
+        message_recu = self.socket.recv(1024)
+        print(message_recu)
