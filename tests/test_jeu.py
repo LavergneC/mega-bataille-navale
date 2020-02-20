317c4592ee8825c8e8d1cea42788b7a4413cfac7
nicolas_gibaud <n.gibaud@catie.fr>
2020-02-20 12:58:12 +0100

    Add some tests for parse_message

--- tests/test_jeu.py
+++ tests/test_jeu.py
@@ -1,4 +1,5 @@
 import sys
+import pytest
 
 sys.path.insert(1, "src")
 
@@ -30,67 +31,26 @@ def test_recevoir_tir():
     assert jeu.recevoir_tir(11, 5) == (True, 1)
     assert jeu.recevoir_tir(4, 7) == (True, 2)
 
+liste_parametres = []
+liste_parametres.append(([2, 1, 5], (1, 5)))
+liste_parametres.append(([0, 1, 1], (None, None)))
+liste_parametres.append(([2, 0, 5], (0, 5)))
+liste_parametres.append(([2, 5, 17], (5, 17)))
+liste_parametres.append(([2, 18, 0], (18, 0)))
+liste_parametres.append(([-5, 18, 0], (None, None)))
+liste_parametres.append(([2, -5, 15], (-5, 15)))
+liste_parametres.append(([2.0, 2.5, 5.5], (2.5, 5.5)))
 
-def test_parse_message():
+liste_parametres.append(([3, 0, 1], ("Rate", "Non_coule")))
+liste_parametres.append(([3, 1, 1], ("Touche_bateau", "Non_coule")))
+liste_parametres.append(([3, 2, 1], ("Touche_sous_marin_surface", "Non_coule")))
+liste_parametres.append(([3, 3, 0], ("Touche_sous_marin_profond", "Coule")))
+
+@pytest.mark.parametrize("test_input,expected", liste_parametres)
+def test_parse_message(test_input, expected):
     """Test de la fonction parse_message."""
     jeu = Jeu()
-
-    trame = []
-    trame.append(2)
-    trame.append(1)
-    trame.append(5)
-    assert jeu.parse_message(trame) == (1, 5)
-
-    #  Bad Message ID
-    trame1 = []
-    trame1.append(0)
-    trame1.append(1)
-    trame1.append(1)
-    assert jeu.parse_message(trame1) == (None, None)
-
-    trame2 = []
-    trame2.append(2)
-    trame2.append(0)
-    trame2.append(5)
-    assert jeu.parse_message(trame2) == (0, 5)
-
-    trame3 = []
-    trame3.append(2)
-    trame3.append(5)
-    trame3.append(17)
-    assert jeu.parse_message(trame3) == (5, 17)
-
-    trame4 = []
-    trame4.append(2)
-    trame4.append(18)
-    trame4.append(0)
-    assert jeu.parse_message(trame4) == (18, 0)
-
-    #  Negative message ID
-    trame5 = []
-    trame5.append(-5)
-    trame5.append(18)
-    trame5.append(0)
-    assert jeu.parse_message(trame5) == (None, None)
-
-    trame6 = []
-    trame6.append(2)
-    trame6.append(-5)
-    trame6.append(15)
-    assert jeu.parse_message(trame6) == (-5, 15)
-
-    trame8 = []
-    trame8.append(2.0)
-    trame8.append(2.5)
-    trame8.append(5.5)
-    assert jeu.parse_message(trame8) == (2.5, 5.5)
-
-    # trame9 = []
-    # trame9.append(a)
-    # trame9.append(5)
-    # trame9.append(b)
-    # assert jeu.parse_message(trame9) == False
-
+    assert jeu.parse_message(test_input) == expected
 
 def test_placer_navire():
     jeu = Jeu()
