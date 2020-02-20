5e82e29376be84071137437a48ee666d4b12c8ce
LavergneC <lavergne.clement@yahoo.fr>
2020-02-20 16:25:01 +0100

    new tests

--- tests/test_jeu.py
+++ tests/test_jeu.py
@@ -111,3 +111,45 @@ def test_placer_navire():
     assert not jeu.carte_perso.navires[0].cases[2].x == 6
     assert not jeu.carte_perso.navires[0].cases[2].y == 8
     assert not jeu.carte_perso.navires[0].cases[1].z == 2
+
+    jeu.placer_navire(0,0,1,"Horizontal", "sous-marins-nuc")
+    assert jeu.carte_perso.navires[1].cases[1].x == 1
+    assert jeu.carte_perso.navires[1].cases[1].y == 0
+    assert jeu.carte_perso.navires[1].cases[1].z == 1
+
+def test_get_navire_at():
+    jeu = Jeu()
+
+    jeu.placer_navire(2,2,2,"Horizontal", "sous-marins")
+    assert jeu.get_navire_at(32, 2) is True
+    assert jeu.get_navire_at(33, 2) is True
+
+    assert jeu.get_navire_at(32, 1) is False
+    assert jeu.get_navire_at(33, 0) is False
+
+    assert jeu.get_navire_at(31, 2) is False
+    assert jeu.get_navire_at(34, 2) is False
+
+def test_defense_touche():
+    jeu = Jeu()
+
+    jeu.placer_navire(1,2,1,"Horizontal", "petit-sous-marins")
+    jeu.recevoir_tir(0,0)
+    jeu.recevoir_tir(1,2)
+    jeu.recevoir_tir(3,3)
+
+    assert jeu.get_defense_touche(0, 2) is True
+    assert jeu.get_defense_touche(0, 1) is True
+    assert jeu.get_defense_touche(0, 0) is True   
+
+    assert jeu.get_defense_touche(31, 2) is False
+    assert jeu.get_defense_touche(31, 1) is True
+    assert jeu.get_defense_touche(31, 0) is True 
+
+    assert jeu.get_defense_touche(32, 2) is False
+    assert jeu.get_defense_touche(32, 1) is False
+    assert jeu.get_defense_touche(32, 0) is False 
+
+    assert jeu.get_defense_touche(48, 2) is False
+    assert jeu.get_defense_touche(48, 1) is True
+    assert jeu.get_defense_touche(48, 0) is True   
