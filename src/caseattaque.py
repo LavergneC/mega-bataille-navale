6a60b6dc567d34fe36cf116363e677e499045bc0
nicolas_gibaud <n.gibaud@catie.fr>
2020-02-21 12:44:19 +0100

    Add documentation for every method

--- src/caseattaque.py
+++ src/caseattaque.py
@@ -2,8 +2,17 @@ from case import Case
 
 
 class CaseAttaque(Case):
-    """Enfant de la classe Case"""
+    """Instance d'une case spécialisée pour la carte de type attaque héritant de Case"""
 
     def __init__(self, x, y, z):
+        """Initie une case
+
+        Parameters:
+            x (int): abscisse de la case (0 <= x < 15)
+            y (int): ordonnée de la case (0 <= y < 15)
+            z (int): profondeur de la case (0 <= z < 3)
+
+         """
+
         super().__init__(x, y, z)
         self.presence_bateau = False
