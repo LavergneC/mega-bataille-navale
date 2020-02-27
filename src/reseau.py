9a7c8754a5eb998b0243289d47b0ba4a18b62351
nicolas_gibaud <n.gibaud@catie.fr>
2020-02-27 17:28:30 +0100

    Get ip local

--- src/reseau.py
+++ src/reseau.py
@@ -16,7 +16,7 @@ class Reseau:
         """Attribue la valeur de l'adresse IP de l'hôte à l'attribut
            concerné."""
         hostname = socket.gethostname()
-        self.ip_address = socket.gethostbyname(hostname)
+        self.ip_address = socket.gethostbyname(hostname+".local")
 
     def se_connecter(self, ip, port):
         """ (Client) Se connecte à l'hôte auquel appartient l'adresse IP
